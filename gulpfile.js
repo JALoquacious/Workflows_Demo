926c4a3d5c6d23f9cde35879e833a1adf55ef85a
JALoquacious <jalikoudis@protonmail.com>
2017-05-25 15:59:14 -0400

    Added Compass, Connect, and Watch to Gulp; added more Images

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp'),
     gUtil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     concat = require('gulp-concat'),
+    compass = require('gulp-compass'),
+    connect = require('gulp-connect'),
     browserify = require('gulp-browserify');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
@@ -11,6 +13,9 @@ var jsSources = [
     'components/scripts/tagline.js',
     'components/scripts/template.js',
 ];
+var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 
 gulp.task('coffee', function() {
     gulp.src(coffeeSources)
@@ -20,9 +25,48 @@ gulp.task('coffee', function() {
 });
 
 gulp.task('js', function() {
-    gUtil.log('The task of outputting script.js is running.')
     gulp.src(jsSources)
         .pipe(concat('script.js'))
         .pipe(browserify())
         .pipe(gulp.dest('builds/development/js'))
+        .pipe(connect.reload())
 });
+
+gulp.task('compass', function() {
+    gulp.src(sassSources)
+        .pipe(compass({
+            sass: 'components/sass',
+            image: 'builds/development/images',
+            style: 'expanded'
+        })
+        .on('error', gUtil.log))
+        .pipe(gulp.dest('builds/development/css'))
+        .pipe(connect.reload())
+});
+
+gulp.task('watch', function() {
+    gulp.watch(coffeeSources, ['coffee']);
+    gulp.watch(jsSources, ['js']);
+    gulp.watch('components/sass/*.scss', ['compass']);
+    gulp.watch(htmlSources, ['html'])
+    gulp.watch(htmlSources, ['json'])
+});
+
+gulp.task('connect', function() {
+    connect.server({
+        root: 'builds/development/',
+        livereload: true
+    });
+});
+
+gulp.task('html', function() {
+    gulp.src(htmlSources)
+        .pipe(connect.reload())
+});
+
+gulp.task('json', function() {
+    gulp.src(jsonSources)
+        .pipe(connect.reload())
+});
+
+gulp.task('all', ['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']);
