eb4172750a96cb905c836ea65ef20249a5169698
JALoquacious <jalikoudis@protonmail.com>
2017-05-25 14:26:50 -0400

    Added Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,28 @@
 var gulp = require('gulp'),
     gUtil = require('gulp-util'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat'),
+    browserify = require('gulp-browserify');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
+var jsSources = [
+    'components/scripts/rclick.js',
+    'components/scripts/pixgrid.js',
+    'components/scripts/tagline.js',
+    'components/scripts/template.js',
+];
 
 gulp.task('coffee', function() {
     gulp.src(coffeeSources)
         .pipe(coffee({ bare: true })
             .on('error', gUtil.log))
         .pipe(gulp.dest('components/scripts'))
+});
+
+gulp.task('js', function() {
+    gUtil.log('The task of outputting script.js is running.')
+    gulp.src(jsSources)
+        .pipe(concat('script.js'))
+        .pipe(browserify())
+        .pipe(gulp.dest('builds/development/js'))
 });
